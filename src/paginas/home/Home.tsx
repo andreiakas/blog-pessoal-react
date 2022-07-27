b0512c0f23a9f1a5382616a9d154fad834c131e6
Andreia <deia_kas@hotmail.com>
2022-07-27 11:20:21 -0300

    iniciando projeto

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+        <> 
+            <h1 className="titulo">Home</h1>
+            <img src= 'https://i.imgur.com/anv76fv.jpeg' alt='Imagem do espaço' className="img"/>
+        </>
+    );
+}
+
+export default Home;
